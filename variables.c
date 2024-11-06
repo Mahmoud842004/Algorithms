ebc7f91a5760d7080e74f2f1adc5c1f23f06f4cb
unknown <bodyashhegab@gmail.com>
2024-11-06 04:30:35 +0200

    making insertion algorithms using nested for loop

--- variables.c
+++ variables.c
@@ -1,21 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main()
+{
+
+ int array [] = {1,3,5,2,4,6};
+ 
+ int lenght = sizeof(array)/sizeof(array[0]);
+
+ int insertion = 0;
+
+
+ for (int i = 0; i < lenght-1 ; i++)
+ {
     
-// let vce = 0.2 
-double ib;
-double ic;
-double beta;
-int vcc;
+   for(int j = i+1;j<lenght;j++)
+    {
+      if (array[i]>array[j])
+      {
+        ++insertion;
+      }
+      
+    }
+    
+ }
+ printf("%d\n",insertion);
 
-scanf("%d",&vcc);
 
-ib = (vcc-0.7)/(122500);
-ic = (vcc-0.2)/(1000);
-beta = ic/ib;
-
-printf("%d",beta);
-
+ 
 }
-
-
